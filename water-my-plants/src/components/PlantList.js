f1e79146b6b6590d8c7a1fb3657269c8d8486ed5
coriagogo <pkcparis@gmail.com>
2019-05-20 18:25:48 -0400

    plant and plantlist components added

--- water-my-plants/src/components/PlantList.js
+++ water-my-plants/src/components/PlantList.js
@@ -1,1 +1,23 @@
 import React from 'react';
+
+
+class PlantList extends React.Component {
+    constructor(props) {
+        super(props)
+        this.state = {
+            plants: []
+        };
+    }
+
+    render() {
+        return(
+            <PlantsWrapper>
+                {this.props.map(plant => (
+                    <Plant key={plant.id} plant={plant} deletePlant={this.deletePlant} editPlant={this.editPlant} />
+                ))}
+            </PlantsWrapper>
+        )
+    }
+}
+
+export default PlantList;
