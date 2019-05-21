ec65a5a526ccc64bfd11ec391a76944ded4db2e3
coriagogo <pkcparis@gmail.com>
2019-05-21 16:23:32 -0400

    added stuff will be changing repos

---
+++ water-my-plants/src/components/Registration.js
@@ -0,0 +1,110 @@
+import React from 'react';
+import { connect } from 'react-redux';
+import { registration } from '../actions/actions';
+import Loader from "react-loader-spinner";
+import styled from 'styled-components';
+
+const SignUpStyles = styled.div`
+    width: 275px;
+    height: 300px;
+    background-color: whitesmoke;
+    margin: 50px auto;
+    border-radius: 15px;
+`
+
+const SignUpForm = styled.form`
+    font-family: 'Roboto', sans-serif;
+    display: flex;
+    flex-direction: column;
+    width: 250px;
+    margin: 0 auto;
+    
+
+    input {
+        margin: 10px;
+        height: 35px;
+        border: none;
+        border-radius: 20px;
+        padding-top: 5px;
+        
+
+        :first-child {
+            margin-top: 25px;
+        }    
+
+        ::placeholder {
+            font-size: 25px;
+            text-align: center; 
+            color: hsla(149, 35%, 45%, 1);       
+        }
+
+    }
+`
+
+class Registration extends React.Component {
+
+    state = {
+        credentials: {
+            username: '',
+            password: '',
+            phoneNumber: ''
+        }
+    }
+
+    handleChange = e => {
+        this.setState({ 
+            credentials: {
+                ...this.state.credentials,
+                [e.target.name]: e.target.value
+                }
+            })
+    }
+
+    registration = e => {
+        e.preventDefault();
+        this.props.registration(this.state.credentials)
+        .then(() => {
+            this.props.history.push('/protected');
+          });
+    };
+
+    render(){
+        return (
+            <SignUpStyles>
+                <SignUpForm onSubmit={this.registration}>
+                    <input 
+                        type="text"
+                        name="username"
+                        placeholder="Username"
+                        value={this.state.credentials.username}
+                        onChange={this.handleChange}
+                    />
+                    <input 
+                        type="password"
+                        name="password"
+                        placeholder="Password"
+                        value={this.state.credentials.password}
+                        onChange={this.handleChange}
+                    />
+                    <input 
+                        type="text"
+                        name="phoneNumber"
+                        placeholder="Phone Number"
+                        value={this.state.credentials.phoneNumber}
+                        onChange={this.handleChange}
+                    />
+                    <button>
+                        {this.props.isRegistering 
+                            ? <Loader type="ThreeDots" color="#156ac1" height="25" width="50" /> 
+                            : "Sign Up"}
+                    </button>
+                </SignUpForm>
+            </SignUpStyles>
+        )
+    }
+
+}
+const mapStateToProps = state => ({
+    isRegistering: state.isRegistering
+})
+export default connect(mapStateToProps, { registration })(Registration)
--- water-my-plants/src/index.js
+++ water-my-plants/src/index.js
@@ -1,19 +1,21 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { BrowserRouter as Router, withRouter } from 'react-router-dom';
 import './index.css';
-import App from './App';
+import App from './components/App';
+import thunk from 'redux-thunk';
+import logger from 'redux-logger';
+import { createStore, applyMiddleware } from 'redux';
+import { Provider } from 'react-redux';
+import reducer from './reducers/reducer';
 
-const AppWithRouter = withRouter(App);
-
+const store = createStore(
+  reducer, 
+  applyMiddleware(thunk, logger)
+);
 
 ReactDOM.render(
-    <Router>
-        <AppWithRouter />
-    </Router>,
-    document.getElementById('root'));
-
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  document.getElementById('root')
+);
